341fea7d52ab16d0af4b66f0fa0d04e9f8744bad
Sam Light <sam@lightscale.co.uk>
2018-05-24 15:56:46 +0100

    Added overlay drag over allowed moves.

--- src/sol_clj/drag_n_drop.cljs
+++ src/sol_clj/drag_n_drop.cljs
@@ -7,47 +7,51 @@
       (.getData "application/edn")
       reader/read-string))
 
+(defn- targ-match? [ev] (= (.-target ev) (.-currentTarget ev)))
+(defn- ev-elem [ev] (.-currentTarget ev))
+
 (defn drag-start [value location ev]
-  (set! (-> ev .-dataTransfer .-dropEffect) "move")
-  (let [target (.-currentTarget ev)]
-    (when (= (.-target ev) (.-currentTarget ev))
+  (when (targ-match? ev)
+    (let [target (.-currentTarget ev)]
       (.requestAnimationFrame
        js/window
-       #(set! (-> target .-style .-opacity) "0"))
-      (-> ev .-dataTransfer
-          (.setData
-           "application/edn"
-           (pr-str {:value value
-                    :location location}))))))
+       #(set! (-> target .-style .-opacity) "0")))
+    (set! (-> ev .-dataTransfer .-dropEffect) "move")
+    (-> ev .-dataTransfer
+        (.setData
+         "application/edn"
+         (pr-str {:value value
+                  :location location})))))
 
 (defn drag-end [ev]
-  (let [target (.-currentTarget ev)]
-    (.requestAnimationFrame
-     js/window
-     #(set! (-> target .-style .-opacity) "1")
-     500)))
+  (when (targ-match? ev)
+    (let [targ (ev-elem ev)]
+      (.requestAnimationFrame
+       js/window
+       #(set! (-> targ .-style .-opacity) "1")
+       500))))
 
 (defn drop-check [state location ev]
-  (if (c/card-drop-allow
-       @state
-       (-> ev get-drag-data :location)
-       location)
+  (.log js/console ev)
+  (when (c/card-drop-allow
+         @state
+         (-> ev get-drag-data :location)
+         location)
     (.preventDefault ev)))
+
+(defn drag-enter [state location ev]
+  (when  (c/card-drop-allow
+          @state
+          (-> ev get-drag-data :location)
+          location)
+    (.preventDefault ev)
+    (-> ev ev-elem .-classList (.add "drag-over"))))
+
+(defn drag-exit [ev]
+  (-> ev ev-elem .-classList (.remove "drag-over")))
 
 (defn on-drop [state location ev]
   (.preventDefault ev)
-  (let [{cloc :location} (get-drag-data ev)
-        c (get-in @state cloc)
-        c' (if (int? c) [c true] c)
-        srccolloc (butlast cloc)
-        srccol (get-in @state srccolloc)
-        tomove (if (int? c)
-                 [c']
-                 (subvec srccol (last cloc)))
-        nsrccol (subvec srccol 0 (last cloc))
-        nsrccol' (if (-> nsrccol first int?)
-                  nsrccol
-                  (conj (vec (butlast nsrccol))
-                        [(first (last nsrccol)) true]))]
-    (swap! state assoc-in srccolloc nsrccol')
-    (swap! state update-in location into tomove)))
+  (-> ev ev-elem .-classList (.remove "drag-over"))
+  (let [{cloc :location} (get-drag-data ev)]
+    (c/move-card state cloc location)))
